78eb151e7c70806c8707ed2229e0f4ec3286aefe
Jat15 <jinan15@hotmail.com>
2017-08-28 08:34:18 +0200

    Aged Old Mis a jour

--- mods/age_old/init.lua
+++ mods/age_old/init.lua
@@ -10,13 +10,221 @@ Code et graphismes sous licence CreativeCommons by-nc 4.0
 dofile(minetest.get_modpath("age_old").."/crafting.lua")
 
 
-minetest.register_node("age_old:sandstone_aged", {
+local function age_old_node_stairs(nodename, def)	
+	local mod = string.match (nodename,"(.+):")
+	local name = string.match (nodename,":(.+)")
+	minetest.register_node(nodename,def)
+	if minetest.get_modpath("moreblocks") then
+		stairsplus:register_all(
+			mod,
+			name,
+			nodename,
+			{
+				description = def.description,
+				tiles = def.tiles,
+				groups = def.groups,
+				sounds = def.sounds,
+			}
+		)
+	elseif minetest.get_modpath("stairs") then	
+		stairs.register_stair_and_slab(name,nodename,
+			def.groups,
+			def.tiles,
+			S("%s Stair"):format(S(def.description)),
+			S("%s Slab"):format(S(def.description)),
+			def.sounds
+		)	
+	end	
+end
+
+
+age_old_node_stairs("age_old:sandstone_aged", {
 	description = "Sandstone_aged",
 	tiles = {"age_old_sandstone_aged.png"},
 	is_ground_content = true,
 	groups = {cracky=3},
 	sounds = default.node_sound_stone_defaults()
 })
+
+age_old_node_stairs("age_old:sandstone_bricks", {
+	description = "Sandstone_bricks",
+	tiles = {"age_old_sandstone_bricks.png"},
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:sandstone_bricks_alt", {
+	description = "Sandstone_bricks_alt",
+	tiles = {"age_old_sandstone_bricks_alt.png"},
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:sandstone_cracked", {
+	description = "Sandstone_cracked",
+	tiles = {"age_old_sandstone_cracked.png"},
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:sandstone_double_slab", {
+	description = "Sandstone_double_slab",
+	tiles = {"age_old_sandstone_double_slab_top.png",
+"age_old_sandstone_double_slab.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:sandstone_facade", {
+	description = "Sandstone_facade",
+	tiles = {"age_old_sandstone_facade_top.png",
+"age_old_sandstone_facade.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:sandstone_floor", {
+	description = "Sandstone_floor",
+	tiles = {"age_old_sandstone_floor.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:sandstone_pillar", {
+	description = "Sandstone_pillar",
+	tiles = {"age_old_sandstone_pillar_top.png",
+"age_old_sandstone_pillar.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:sandstone_tile", {
+	description = "Sandstone_tile",
+	tiles = {"age_old_sandstone_tile.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:sandstone_wall_frieze", {
+	description = "Sandstone_wall_frieze",
+	tiles = {"age_old_sandstone_wall_frieze_top.png",
+"age_old_sandstone_wall_frieze.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:stone_aged", {
+	description = "Stone_aged",
+	tiles = {"age_old_stone_aged.png"},
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:stone_aged_cracked", {
+	description = "Stone_aged_cracked",
+	tiles = {"age_old_stone_aged_cracked.png"},
+	is_ground_content = true,
+	groups = {cracky=3},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:stone_aged_double_slab", {
+	description = "Stone_aged_double_slab",
+	tiles = {"age_old_stone_aged_double_slab_top.png",
+"age_old_stone_aged_double_slab.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:stone_aged_floor", {
+	description = "Stone_aged_floor",
+	tiles = {"age_old_stone_aged_floor.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()	
+})
+
+age_old_node_stairs("age_old:stone_aged_tile", {
+	description = "Stone_aged_tile",
+	tiles = {"age_old_stone_aged_tile.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:stonebricks_aged", {
+	description = "Stonebricks_aged",
+	tiles = {"age_old_stonebricks_aged.png"},
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:stonebricks_aged_alt", {
+	description = "Stonebricks_aged_alt",
+	tiles = {"age_old_stonebricks_aged_alt.png"},
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:stonebricks_aged_mossy", {
+	description = "Stonebricks_aged_mossy",
+	tiles = {"age_old_stonebricks_aged_mossy.png"},
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:stone_aged_pavement", {
+	description = "Stone_aged_pavement",
+	tiles = {"age_old_stone_aged_pavement.png"},
+	paramtype2 = "facedir",
+	place_param2 = 0,
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:stone_aged_facade", {
+	description = "Stone_aged_facade",
+	tiles = {"age_old_stone_aged_facade.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+age_old_node_stairs("age_old:stone_aged_pillar", {
+	description = "Stone_aged_pillar",
+	tiles = {"age_old_stone_aged_pillar_top.png",
+"age_old_stone_aged_pillar.png"},
+	is_ground_content = true,
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults()
+})
+
+-- Balustrade 
 
 minetest.register_node("age_old:sandstone_aged_baluster", {
 	description = "Sandstone_aged_baluster",
@@ -26,6 +234,8 @@ minetest.register_node("age_old:sandstone_aged_baluster", {
 	groups = {cracky=3},
 	sounds = default.node_sound_stone_defaults()
 })
+
+-- Bas relief
 
 minetest.register_node("age_old:sandstone_basrelief", {
 	description = "Sandstone_basrelief",
@@ -51,184 +261,6 @@ minetest.register_node("age_old:sandstone_basrelief3", {
 "age_old_sandstone_basrelief3.png"},
 	is_ground_content = true,
 	groups = {cracky=3},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:sandstone_bricks", {
-	description = "Sandstone_bricks",
-	tiles = {"age_old_sandstone_bricks.png"},
-	paramtype2 = "facedir",
-	place_param2 = 0,
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:sandstone_bricks_alt", {
-	description = "Sandstone_bricks_alt",
-	tiles = {"age_old_sandstone_bricks_alt.png"},
-	paramtype2 = "facedir",
-	place_param2 = 0,
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:sandstone_cracked", {
-	description = "Sandstone_cracked",
-	tiles = {"age_old_sandstone_cracked.png"},
-	is_ground_content = true,
-	groups = {cracky=3},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:sandstone_double_slab", {
-	description = "Sandstone_double_slab",
-	tiles = {"age_old_sandstone_double_slab_top.png",
-"age_old_sandstone_double_slab.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:sandstone_facade", {
-	description = "Sandstone_facade",
-	tiles = {"age_old_sandstone_facade_top.png",
-"age_old_sandstone_facade.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:sandstone_floor", {
-	description = "Sandstone_floor",
-	tiles = {"age_old_sandstone_floor.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:sandstone_pillar", {
-	description = "Sandstone_pillar",
-	tiles = {"age_old_sandstone_pillar_top.png",
-"age_old_sandstone_pillar.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:sandstone_tile", {
-	description = "Sandstone_tile",
-	tiles = {"age_old_sandstone_tile.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:sandstone_wall_frieze", {
-	description = "Sandstone_wall_frieze",
-	tiles = {"age_old_sandstone_wall_frieze_top.png",
-"age_old_sandstone_wall_frieze.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:stone_aged", {
-	description = "Stone_aged",
-	tiles = {"age_old_stone_aged.png"},
-	is_ground_content = true,
-	groups = {cracky=3},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:stone_aged_cracked", {
-	description = "Stone_aged_cracked",
-	tiles = {"age_old_stone_aged_cracked.png"},
-	is_ground_content = true,
-	groups = {cracky=3},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:stone_aged_double_slab", {
-	description = "Stone_aged_double_slab",
-	tiles = {"age_old_stone_aged_double_slab_top.png",
-"age_old_stone_aged_double_slab.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:stone_aged_floor", {
-	description = "Stone_aged_floor",
-	tiles = {"age_old_stone_aged_floor.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()	
-})
-
-minetest.register_node("age_old:stone_aged_tile", {
-	description = "Stone_aged_tile",
-	tiles = {"age_old_stone_aged_tile.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:stonebricks_aged", {
-	description = "Stonebricks_aged",
-	tiles = {"age_old_stonebricks_aged.png"},
-	paramtype2 = "facedir",
-	place_param2 = 0,
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:stonebricks_aged_alt", {
-	description = "Stonebricks_aged_alt",
-	tiles = {"age_old_stonebricks_aged_alt.png"},
-	paramtype2 = "facedir",
-	place_param2 = 0,
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:stonebricks_aged_mossy", {
-	description = "Stonebricks_aged_mossy",
-	tiles = {"age_old_stonebricks_aged_mossy.png"},
-	paramtype2 = "facedir",
-	place_param2 = 0,
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:stone_aged_pavement", {
-	description = "Stone_aged_pavement",
-	tiles = {"age_old_stone_aged_pavement.png"},
-	paramtype2 = "facedir",
-	place_param2 = 0,
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:stone_aged_facade", {
-	description = "Stone_aged_facade",
-	tiles = {"age_old_stone_aged_facade.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
-	sounds = default.node_sound_stone_defaults()
-})
-
-minetest.register_node("age_old:stone_aged_pillar", {
-	description = "Stone_aged_pillar",
-	tiles = {"age_old_stone_aged_pillar_top.png",
-"age_old_stone_aged_pillar.png"},
-	is_ground_content = true,
-	groups = {cracky=2},
 	sounds = default.node_sound_stone_defaults()
 })
 
