dc66859794230e2872efc24c9b1af5b1f5b0e4c5
Jat15 <jinan15@hotmail.com>
2017-09-25 15:49:55 +0200

    Mise a jour speedwall (optimisation)

--- mods/speedwall/init.lua
+++ mods/speedwall/init.lua
@@ -11,24 +11,12 @@ code et graphisme sous licence GPL v2 ou superieur
 --déclaration de la liste de couleurs ( dyes et differentes couleur de bloc )
 local couleurs = {"black","blue","brown","cyan","green","grey","orange","pink","red","violet","white","yellow"}
 
-
-
-function CreationDeBlock(couleur, TypeDeBlock, Couper)
-	
-	--Serilize et de deserialize pour enlever tout lien avec les tables
-	function CopieTable(Table)	
-		return minetest.deserialize(minetest.serialize(Table))
-	end
-	
-	--Definition du block de base
-	local BlockDeBase = minetest.registered_nodes["default:"..TypeDeBlock]
+local function CreationDeBlock(couleur, TypeDeBlock, BlockDeBase, Couper)
 	
 	-- Ajoute du groups auquelle il appartien si ce n est pas deja fait
 	if not( BlockDeBase["groups"][TypeDeBlock]== 1) then
 		BlockDeBase["groups"][TypeDeBlock] = 1
 	 end
-	
-	BlockDeBase = CopieTable(BlockDeBase)
 	
 	--Craft
 	minetest.register_craft({
@@ -45,7 +33,6 @@ function CreationDeBlock(couleur, TypeDeBlock, Couper)
 	minetest.register_node("speedwall:"..TypeDeBlock..couleur, BlockDeBase)
 
 	--Retiré le groups	
-	BlockDeBase = CopieTable(BlockDeBase)
 	BlockDeBase["groups"][TypeDeBlock] = nil
 
 	--Création moreblocks
@@ -55,11 +42,34 @@ function CreationDeBlock(couleur, TypeDeBlock, Couper)
 	
 end
 
-
 for z=1,table.getn(couleurs) do
-	CreationDeBlock(couleurs[z],"sand",false)
-	CreationDeBlock(couleurs[z],"sandstone",true)
-	CreationDeBlock(couleurs[z],"sandstonebrick",true)	
+	CreationDeBlock(
+		couleurs[z],
+		"sand",
+		{
+			groups = {crumbly = 3, falling_node = 1, sand = 1},
+			sounds = default.node_sound_sand_defaults(),
+		},
+		false
+	)
+	CreationDeBlock(
+		couleurs[z],
+		"sandstone",
+		{
+			groups = {crumbly = 1, cracky = 3},
+			sounds = default.node_sound_stone_defaults(),
+		},
+		true
+	)
+	CreationDeBlock(
+		couleurs[z],
+		"sandstonebrick",
+		{
+			groups = {cracky = 2},
+			sounds = default.node_sound_stone_defaults(),
+		},
+		true
+	)	
 end
 
 minetest.register_alias("simplewall:bitume", "speedwall:sandstoneblack")
