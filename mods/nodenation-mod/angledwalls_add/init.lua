5beea132389adbe32713ff626336e951900933b6
Jat15 <jinan15@hotmail.com>
2018-11-04 22:14:52 +0100

    Ajout de angledwall_add

---
+++ mods/nodenation-mod/angledwalls_add/init.lua
@@ -0,0 +1,12 @@
+angledwalls.register_angled_wall_and_low_angled_wall_and_corner(
+	"clay", 
+	"default:clay",
+	"Clay Angled Wall",
+	"Clay Low Angled Wall",
+	"Clay Corner",
+	{
+		tiles = {"default_clay.png"},
+		groups = {crumbly = 3},
+		sounds = default.node_sound_dirt_defaults()
+	}
+)
